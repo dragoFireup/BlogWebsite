cdd7b3090a05c7a8c3c14da1db355cfdd9b7216d
R Vrishab Jain <jainvrishab@gmail.com>
2019-11-27 11:37:16 +0530

    Fixed email issues

--- BlogWebsite/settings.py
+++ BlogWebsite/settings.py
@@ -128,3 +128,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'forinternpurposes@gmail.com'
+EMAIL_HOST_PASSWORD = 'qwerty@12345'
